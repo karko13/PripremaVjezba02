a9a10e98a4cadd288bd5dc94f588b5b085a80cee
karko13 <145532713+karko13@users.noreply.github.com>
2023-11-08 09:21:11 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PripremaVjezba01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Hrvatska ima more";
+            string s2 = "Italija je prvak svijeta" ;
+            string s3 = s1.Substring(0,8) + s2.Substring(7, 17);
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+            Console.ReadKey();
+        }
+    }
+}
